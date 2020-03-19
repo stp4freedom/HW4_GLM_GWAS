95b229cf1333d1eb54b1813e1b2593343c8731db
stp4freedom <stp4freedom@gmail.com>
2020-03-19 13:30:39 -0700

    1;31

--- R/Function_fix_Dep.R
+++ R/Function_fix_Dep.R
@@ -3,7 +3,6 @@
 #' @param X1 A matrix.
 #' @param X2 A matrix, the columns of which may be partially linearly dependent on the columns of X1.
 #' @param tol The tolerance to use when assessing linear dependence.
-#' @param The tolerance to use when assessing linear dependence.
 #' @param rank.def If the degree of rank deficiency in X2, given X1, is known, then it can be supplied here, and tol is then ignored. Unused unless positive and not greater than the number of columns in X2.
 #' @param strict if TRUE then only columns individually dependent on X1 are detected, if FALSE then enough columns to make the reduced X2 full rank and independent of X1 are detected.
 #' @return A vector of the columns of X2 which are linearly dependent on columns of X1 (or which need to be deleted to acheive independence and full rank if strict==FALSE). NULL if the two matrices are independent.
